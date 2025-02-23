f107f9c84e370abdd543a3f86cd29a1800679700
Saksham Tewari <sakshamtewari3@gmail.com>
2025-02-23 22:30:23 +0530

    'users' schema

---
+++ src/db/schema.ts
@@ -0,0 +1,16 @@
+import { uniqueIndex } from 'drizzle-orm/mysql-core';
+import { pgTable, uuid, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const users = pgTable(
+  'users',
+  {
+    id: uuid('id').primaryKey().defaultRandom(),
+    clerkId: text('clerk_id').unique().notNull(),
+    name: text('name').notNull(),
+    // TODO: add banner fields
+    imageUrl: text('image_url').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+  },
+  (t) => [uniqueIndex('clerk_id_idx').on(t.clerkId)],
+);
