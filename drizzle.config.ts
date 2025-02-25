e46ebbd47ed807c55b367c44b49412065896e406
Saksham Tewari <sakshamtewari3@gmail.com>
2025-02-25 22:05:52 +0530

    Fixed 'url'
    Co-authored-by: Ravi <ravioli158@users.noreply.github.com>

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,15 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
+// if (!process.env.DATABASE_URL) {
+//   throw new Error('❌ DATABASE_URL is not set in the environment variables.');
+// }
+
 export default defineConfig({
-  out: './drizzle',
+  out: './drizzle/migrations/',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!, //'postgres://user:password@localhost:5432/db'  : ensure 'localhost' is present if hardcoded
   },
 });
