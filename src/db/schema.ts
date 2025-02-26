f54681c1ee2264da1e1d277889e00699aa4414c2
Saksham Tewari <sakshamtewari3@gmail.com>
2025-02-26 21:25:22 +0530

    removed clerk, updated 'users'

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,16 +1,17 @@
-import { uniqueIndex } from 'drizzle-orm/mysql-core';
+import { uniqueIndex, varchar } from 'drizzle-orm/mysql-core';
 import { pgTable, uuid, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const users = pgTable(
   'users',
   {
     id: uuid('id').primaryKey().defaultRandom(),
-    clerkId: text('clerk_id').unique().notNull(),
+    email: varchar('email', { length: 255 }).unique().notNull(),
+    password: text('password').notNull(),
     name: text('name').notNull(),
     // TODO: add banner fields
     imageUrl: text('image_url').notNull(),
     createdAt: timestamp('created_at').defaultNow().notNull(),
     updatedAt: timestamp('updated_at').defaultNow().notNull(),
   },
-  (t) => [uniqueIndex('clerk_id_idx').on(t.clerkId)],
+  //(t) => [uniqueIndex('clerk_id_idx').on(t.clerkId)],
 );
