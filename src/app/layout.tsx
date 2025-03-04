8469c9f7f5ed3be01a3363806dc2946b6ab93335
Saksham Tewari <sakshamtewari3@gmail.com>
2025-03-04 19:42:43 +0530

    TRPCProvider wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import { TRPCProvider } from '@/trpc/client';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,14 +17,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider afterSignOutUrl='/'>
       <html lang='en' suppressHydrationWarning>
         {/* <body className={inter.className}>{children}</body> */}
         <body
           className={`${inter.className} antialiased`}
           suppressHydrationWarning
         >
-          {children}
+          <TRPCProvider>{children}</TRPCProvider>
         </body>
       </html>
     </ClerkProvider>
