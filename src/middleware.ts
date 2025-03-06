5137c02a561394ea74d5792984d4203e47fa99e1
Saksham Tewari <sakshamtewari3@gmail.com>
2025-03-06 18:20:56 +0530

    Middleware : Authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,20 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { verifyToken } from './lib/auth';
+import { parse } from 'cookie';
 
-export default clerkMiddleware();
+export function middleware(req: NextRequest) {
+  const cookies = req.headers.get('cookie');
+  const token = cookies ? parse(cookies).token : null;
 
+  if (!token || !verifyToken(token)) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    // return NextResponse.redirect('/signin');
+  }
+
+  return NextResponse.next();
+}
+
+// Apply the middleware to protected routes only
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: '/api/protected/:path*',
 };
